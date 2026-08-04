1e0560d307f1e3891f8fdc98072c2ad013644a02
OKE225 <patryk.wroblewski.2006@gmail.com>
2026-08-04 20:25:14 +0200

    Add some content

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import { Canvas } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import Earth from "./components/Earth.jsx";
 import { Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
-import Earth from "./components/Earth";
+import { Canvas } from "@react-three/fiber";
 import Stars from "./components/Stars";
 
 const App = () => {
   return (
-    <>
-      <Stars />
+    <div className="relative min-h-screen">
+      <div className="fixed inset-0 -z-10">
+        <Stars />
+        <div className="h-full w-full">
+          <Canvas>
+            <PerspectiveCamera
+              makeDefault
+              position={[0, 0.8, 1.5]}
+              fov={45}
+              near={0.1}
+              far={1000}
+            />
+            <ambientLight intensity={2.25} />
+            <OrbitControls
+              enableDamping={false}
+              enablePan={false}
+              enableRotate={false}
+              enableZoom={false}
+              autoRotate
+              autoRotateSpeed={0.5}
+              target={[0, 1.25, 0]}
+            />
+            <Suspense fallback={null}>
+              <Earth />
+            </Suspense>
+          </Canvas>
+        </div>
+      </div>
 
-      <Canvas camera={{ position: [0, 1, 4], fov: 45 }}>
-        <ambientLight intensity={1} />
-        <OrbitControls autoRotate autoRotateSpeed={1} />
-        <Suspense fallback={null}>
-          <Earth />
-        </Suspense>
-      </Canvas>
-    </>
+      <div className="relative z-10 flex h-[50vh] flex-col items-center justify-end">
+        <p className="text-3xl mb-8 font-light text-neutral-400">
+          Our Home in the Cosmos
+        </p>
+        <h1 className="text-[152px] font-libre font-medium text-zinc-50 tracking-wide leading-none">
+          EARTH
+        </h1>
+      </div>
+    </div>
   );
 };
 
